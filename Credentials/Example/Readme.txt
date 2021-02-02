670035fc748fb1d12e237e016637d4c2f2536f63
Andrey Vasilev <ao.vasilev@gmail.com>
2021-02-02 13:03:27 +0300

    Update

--- Credentials/Example/Readme.txt
+++ Credentials/Example/Readme.txt
@@ -1,5 +1,6 @@
 1. Create directory for each project's credentials
 2. Copy to the created directory (i.e. Credentials/Example) Appfile from your project's .fastlane/ directory
 3. Copy to the created directory (i.e. Credentials/Example) GoogleService-Info.plist file
-4. Open the root folder in the Terminal
-5. sh refresh-dsyms.sh
+4. Edit options.plist
+5. Open the root folder in the Terminal
+6. sh refresh-dsyms.sh
--- refresh-dsyms.sh
+++ refresh-dsyms.sh
@@ -10,7 +10,7 @@ exec_directory="."
 appfile="Appfile"
 appfile_path="fastlane/$appfile"
 google_service_info_plist="GoogleService-Info.plist"
-min_version="min_version.txt"
+options_plist="options.plist"
 
 credentials_directory="../Credentials"
 logs_file="../logs.log"
@@ -23,15 +23,15 @@ for project in $credentials_directory/*; do
     cp "$project/Appfile" "$exec_directory/$appfile_path"
     rm -f "$exec_directory/$google_service_info_plist"
     cp "$project/$google_service_info_plist" "$exec_directory/$google_service_info_plist"
-    rm -f "$exec_directory/$min_version"
-    cp "$project/$min_version" "$exec_directory/$min_version"
+    rm -f "$exec_directory/$options_plist"
+    cp "$project/$options_plist" "$exec_directory/$options_plist"
     
     bundle exec fastlane refresh_dsyms
     
     #Clear credentials
     rm -f "$exec_directory/$appfile_path"
     rm -f "$exec_directory/$google_service_info_plist"
-    rm -f "$exec_directory/$min_version"
+    rm -f "$exec_directory/$options_plist"
     
     echo "[$(date '+%Y-%m-%d %H:%M:%S')] Successfully refreshed dsyms of $(basename "$project")" >> $logs_file
 done
